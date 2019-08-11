92ea3872b14027cf9b469f93a2cd255e2e23f6c6
Patbhuminand <47202361+Patbhuminand@users.noreply.github.com>
2019-08-12 02:33:40 +0700

    Update Cal.js

--- Cal.js
+++ Cal.js
@@ -1,148 +1,382 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Button } from 'react-native';
-import { StyleSheet, TouchableOpacity} from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native';
 import { Image } from 'react-native';
 
-export default class FacebookButton extends React.Component {
-
+var count = 0;
+var sum = 0;
+export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { InputNumber1 : 0, InputNumber2: 0, Output: 0 };
+    this.state = {
+      output: '0',
+      output2: '',
+    };
   }
 
-onZero(){
-  var temp = 0;
-  this.setState({Output:temp});
-}
-onOne(){
-  var temp =  "1";
-  this.setState({Output:temp});
-}
+  clearNum(ac) {
+    if (ac == 'AC') {
+      count = 0;
+      this.setState({ output: '0' });
+    }
+  }
 
-onTwo(){
-  var temp =  "2";
-  this.setState({Output:temp});
-}
+  showNum(num) {
+    if (this.state.output == '0') {
+      this.setState({ output: num });
+    } else if (num == '.' && count == 0) {
+      count = 1;
+      this.setState({ output: this.state.output + num });
+    } else if (num != '.') {
+      this.setState({ output: this.state.output + num });
+    }
+  }
 
-onThree(){
-  var temp =  "3";
-  this.setState({Output:temp});
-}
+  sum(y) {
+    if (y == '=') {
+      // if (this.state.output.indexOf('÷')) {
+      //   var res = this.state.output.replace('÷', '/');
+      //   console.log('char', res);
+      //   this.setState({ output: eval(res) });
+      // } else if (this.state.output.indexOf('x')) {
+      //   var res2 = this.state.output.replace('x', '*');
+      //   this.setState({ output: eval(res2) });
+      // } else {
+        this.setState({ output: this.state.output });
+    //   }
+    }
+  }
 
-onFour(){
-  var temp =  "4";
-  this.setState({Output:temp});
-}
-
-onFive(){
-  var temp =  "5";
-  this.setState({Output:temp});
-}
-
-onSix(){
-  var temp =  "6";
-  this.setState({Output:temp});
-}
-
-onSeven(){
-  var temp =  "7";
-  this.setState({Output:temp});
-}
-
-onEight(){
-  var temp =  "8";
-  this.setState({Output:temp});
-}
-
-onNine(){
-  var temp =  "9";
-  this.setState({Output:temp});
-}
+  // sign(x) {
+  //   sum = this.state.output;
+  //   this.setState({ sum });
+  //   console.log('character', sum);
+  //   switch (x) {
+  //     case 'x':
+  //       this.setState({ output: this.state.output2 });
+  //       sum = sum * this.state.output;
+  //       this.setState({ output2: sum });
+  //       break;
+  //     case '÷':
+  //       break;
+  //     case '+':
+  //       break;
+  //     case '-':
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 
   render() {
     return (
-    <LinearGradient
+      <LinearGradient
         colors={['#000000', '#000000', '#000000']}
-        style={{flex: 1}}
-      >
-        <View style={{flex: 1}}> />
-          <Text style={styles.ans} >
-                  {this.state.Output}
-              </Text>
-
-          <View style={{flex: 1, flexDirection: 'row',justifyContent: 'center'}}>
-              <View style={{flex: 1, backgroundColor: '#a5a5a5',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.black}>AC</Text></TouchableOpacity>
+        style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.ans}>{''}</Text>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.ans}>{' ' + this.state.output}</Text>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#a5a5a5',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.clearNum('AC')}>
+                  <Text style={styles.black}>AC</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#a5a5a5',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.black}>+/-</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#a5a5a5',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity style={styles.btn2}>
+                  <Text style={styles.black}>+/-</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#a5a5a5',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.black}>%</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#a5a5a5',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('%')}>
+                  <Text style={styles.black}>%</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#ef9835',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.all}>÷</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#ef9835',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('÷')}>
+                  <Text style={styles.all}>÷</Text>
+                </TouchableOpacity>
               </View>
-          </View>
-
-           <View style={{flex: 1, flexDirection: 'row',justifyContent: 'center'}}>
-             <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onSeven()}><Text style={styles.all}>7</Text></TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('7')}>
+                  <Text style={styles.all}>7</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onEight()}><Text style={styles.all}>8</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('8')}>
+                  <Text style={styles.all}>8</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onNine()}><Text style={styles.all}>9</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('9')}>
+                  <Text style={styles.all}>9</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#ef9835',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.all}>x</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#ef9835',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('x')}>
+                  <Text style={styles.all}>x</Text>
+                </TouchableOpacity>
               </View>
-          </View>
-
-           <View style={{flex: 1, flexDirection: 'row',justifyContent: 'center'}}>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onFour()}><Text style={styles.all}>4</Text></TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('4')}>
+                  <Text style={styles.all}>4</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onFive()} ><Text style={styles.all}>5</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('5')}>
+                  <Text style={styles.all}>5</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onSix()} ><Text style={styles.all}>6</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('6')}>
+                  <Text style={styles.all}>6</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#ef9835',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.all}>-</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#ef9835',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('-')}>
+                  <Text style={styles.all}>-</Text>
+                </TouchableOpacity>
               </View>
-          </View>
-
-          <View style={{flex: 1, flexDirection: 'row',justifyContent: 'center'}}>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onOne()} ><Text style={styles.all}>1</Text></TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('1')}>
+                  <Text style={styles.all}>1</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onTwo()} ><Text style={styles.all}>2</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('2')}>
+                  <Text style={styles.all}>2</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onThree()}><Text style={styles.all}>3</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('3')}>
+                  <Text style={styles.all}>3</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#ef9835',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.all}>+</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#ef9835',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity style={styles.btn2}>
+                  <Text style={styles.all}>+</Text>
+                </TouchableOpacity>
               </View>
-          </View>
-
-          <View style={{flex: 1, flexDirection: 'row',justifyContent: 'center'}}>
-              <View style={{flex: 2, backgroundColor: '#343434',margin:5,borderRadius: 50,height:80}} >
-                <TouchableOpacity style={styles.btn2} onPress = {()=>this.onZero()} ><Text style={styles.zero}>0</Text></TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  flex: 2,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 50,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('0')}>
+                  <Text style={styles.zero}>0</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{flex: 1, backgroundColor: '#343434',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.all}>.</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#343434',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.showNum('.')}>
+                  <Text style={styles.all}>.</Text>
+                </TouchableOpacity>
               </View>
-               <View style={{flex: 1, backgroundColor: '#ef9835',margin:5,borderRadius: 100,height:80}} >
-                <TouchableOpacity style={styles.btn2}><Text style={styles.all}>=</Text></TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: '#ef9835',
+                  margin: 5,
+                  borderRadius: 100,
+                  height: 80,
+                }}>
+                <TouchableOpacity
+                  style={styles.btn2}
+                  onPress={() => this.sum('=')}>
+                  <Text style={styles.all}>=</Text>
+                </TouchableOpacity>
               </View>
+            </View>
           </View>
         </View>
       </LinearGradient>
@@ -151,28 +385,28 @@ onNine(){
 }
 
 const styles = StyleSheet.create({
-  zero:{
-    textAlign:'left',
+  zero: {
+    textAlign: 'left',
     padding: 25,
     fontSize: 30,
-    color:"#ffffff"
+    color: '#ffffff',
   },
-  all:{
-    textAlign:'center',
+  all: {
+    textAlign: 'center',
     padding: 20,
     fontSize: 30,
-    color:"#ffffff"
+    color: '#ffffff',
   },
-  black:{
-    textAlign:'center',
-    padding: 21,
-    fontSize: 20,
-    color:"#000000"
+  black: {
+    textAlign: 'center',
+    padding: 26,
+    fontSize: 24,
+    color: '#000000',
   },
-  ans:{
-    textAlign:'right',
+  ans: {
+    textAlign: 'right',
     padding: 20,
     fontSize: 60,
-    color:"#ffffff"
-  }
-})
+    color: '#ffffff',
+  },
+});
